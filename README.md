6cf548004c09d154c65b944bddb1ea0c3a587a5e
Pedro Scaff <ext-pedro.scaff@here.com>
2017-04-21 13:52:56 +0200

    update to react and preparing for nz like page

---
+++ README.md
@@ -0,0 +1,11 @@
+visualização distribuição racial
+======================================
+
+### Localização dos dados
+
+- `ftp://ftp.ibge.gov.br/`
+    - `/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/`
+
+- ftp://geoftp.ibge.gov.br/
+    - `/organizacao_do_territorio/malhas_territoriais/malhas_de_setores_censitarios__divisoes_intramunicipais/censo_2010/setores_censitarios_shp/
+`
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
+import './index.css';
+import 'whatwg-fetch';
 import {app_id, app_code} from '../datalens.json';
 import {dsvFormat} from 'd3-dsv';
 import {scaleLinear} from 'd3-scale';
-import {scalePow} from 'd3-scale';
+// import {scalePow} from 'd3-scale';
 import {scaleOrdinal} from 'd3-scale';
-import {extent} from 'd3-array';
-import Panel from 'datalens-ui/Panel';
-import Label from 'datalens-ui/Label';
-import ColorLegend from 'datalens-ui/ColorLegend';
-import Select from 'datalens-ui/Select';
+// import {extent} from 'd3-array';
+import UIControls from './UIControls.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 // initialize communication with the platform
 const platform = new H.service.Platform({
@@ -39,7 +40,7 @@ window.addEventListener('resize', function() {
 
 //make the map interactive
 new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-let ui = H.ui.UI.createDefault(map, defaultLayers);
+H.ui.UI.createDefault(map, defaultLayers);
 
 //create providers
 // let service = platform.configure(new H.datalens.Service());
@@ -55,7 +56,7 @@ function addLayer(data) {
         .domain(majorityDomain)
         .range(majorityColors);
 
-    let singleColors = ['#1BD3E0', '#1BA4EA', '#1E63D3', '#6320E0', '#9D1AE0'];
+    let singleColors = ['#1BD3E0', '#9D1AE0'];
 
     function majorityFillColor(id) {
         let idx = data.findIndex(entry => {
@@ -93,22 +94,22 @@ function addLayer(data) {
             },
             defaultStyle: () => {
                 return {
-                    fillColor: 'gray'
+                    opacity: 0.0
                 };
             }
         }
     );
     map.addLayer(majorityLayer);
 
-    function singleFillColor(scale, id) {
+    function singleFillColor(scale, key, id) {
         let idx = data.findIndex(entry => {
             return entry['Cod_setor'] === id;
         });
         if (idx !== -1) {
-            let column = select.getValue();
-            let value = Number.parseInt(data[idx][column]);
+            let value = Number.parseInt(data[idx][key]);
             if (!Number.isNaN(value)) {
-                return scale(value);
+                let percentage = value/Number.parseInt(data[idx]['V001']);
+                return scale(percentage);
             } else {
                 return null;
             }
@@ -135,7 +136,7 @@ function addLayer(data) {
                 },
                 defaultStyle: () => {
                     return {
-                        fillColor: 'gray'
+                        opacity: 0.0
                     };
                 }
             }
@@ -144,76 +145,73 @@ function addLayer(data) {
     }
 
     // create panel
-    const panel = new Panel('Belo Horizonte Data');
-    const censusLabel = new Label();
-    let legendLabels = {
-        '0': 'white',
-        '1': 'black',
-        '2': 'yellow',
-        '3': 'brown',
-        '4': 'indigenous'
-    };
-    const majorityLegend = new ColorLegend(
-        v => majorityColors[Math.floor(v * 5)]
+    let legendLabels = [
+        'white',
+        'black',
+        'yellow',
+        'brown',
+        'indigenous'
+    ];
+    let selectLabels = [
+        {value: 'all', label: 'Show Majority'},
+        {value: 'V002', label: 'white'},
+        {value: 'V003', label: 'black'},
+        {value: 'V004', label: 'yellow'},
+        {value: 'V005', label: 'brown'},
+        {value: 'V006', label: 'indigenous'}
+    ];
+    let uiControls = <UIControls
+        title = 'Belo Horizonte'
+        defaultLabel = {selectLabels[0].label}
+        selectLabels = {selectLabels}
+        onSelectChange = {updateLayers}
+        legendLabels = {legendLabels}
+        scale = {majorityScale}
+    />;
+
+    ReactDOM.render(
+        uiControls,
+        document.getElementById('root')
     );
-    const singleLegend = new ColorLegend(
-        scaleLinear().range([singleColors[0], singleColors[4]]));
-    const select = new Select({
-        'all': 'Show majority',
-        'V002': 'white',
-        'V003': 'black',
-        'V004': 'yellow',
-        'V005': 'brown',
-        'V006': 'indigenous'
-    });
-    ui.addControl('panel', panel);
-    panel.addChild(censusLabel);
-    censusLabel.setHTML('Some census data');
-    panel.addChild(select);
-    panel.addChild(majorityLegend);
-    let majorityLabels = Object.keys(legendLabels).map(
-        key => legendLabels[key]);
-    majorityLegend.setLabels(majorityLabels);
 
-    let infoBubble = new H.ui.InfoBubble({lat: 0, lng: 0}, {});
-    infoBubble.addClass('gdp-info-bubble');
-    infoBubble.close();
-    ui.addBubble(infoBubble);
-
-    function updateLayers() {
+    /**
+     * update layers
+     * @returns {Object} - new labels for the legend, used by uiControls component
+     */
+    function updateLayers(key) {
+        let categories, scale;
         let currentLayers = map.getLayers();
-        let selectedValue = select.getValue();
-        if (selectedValue === 'all') {
+        if (key === 'all') {
             if (currentLayers.indexOf(majorityLayer) === -1) {
                 map.removeLayer(singleLayer);
-                panel.removeChild(singleLegend);
-                panel.addChild(majorityLegend);
                 map.addLayer(majorityLayer);
-                majorityLegend.setLabels(majorityLabels);
             }
+            categories = legendLabels;
+            scale = majorityScale;
+        } else if (key === 'clear') {
+            map.removeLayer(singleLayer);
+            map.removeLayer(majorityLayer);
+            return;
         } else {
             if (currentLayers.indexOf(singleLayer) === -1) {
                 map.removeLayer(majorityLayer);
-                panel.removeChild(majorityLegend);
-                panel.addChild(singleLegend);
-                singleLegend.setLabels(['low', 'high']);
             } else {
                 map.removeLayer(singleLayer);
             }
-            let domain = extent(data, entry => {
-                let value = Number.parseInt(entry[selectedValue]);
-                if (!Number.isNaN(value)) {
-                    return Math.sqrt(value);
-                } else {
-                    return 0;
-                }
-            });
-            let scale = scalePow(0.5).domain(domain).range(singleColors);
-            addSingleLayer(singleFillColor.bind(null, scale));
+            // let domain = extent(data, entry => {
+            //     let value = Number.parseInt(entry[key]);
+            //     if (!Number.isNaN(value)) {
+            //         return Math.sqrt(value);
+            //     } else {
+            //         return 0;
+            //     }
+            // });
+            // let scale = scalePow(0.5).domain(domain).range(singleColors);
+            scale = scaleLinear([0, 1]).range(singleColors);
+            addSingleLayer(singleFillColor.bind(null, scale, key));
         }
+        return {categories: categories, scale: scale};
     }
-
-    select.addEventListener('change', updateLayers);
 
     // let hoveredObject;
     // map.addEventListener('pointermedObject !== e.target) {
@@ -248,11 +246,13 @@ function addLayer(data) {
     // });
 }
 
-let xhr = new XMLHttpRequest();
-xhr.addEventListener('load', function () {
-    if (this.readyState === XMLHttpRequest.DONE) {
-        addLayer(dsvFormat(';').parse(this.responseText));
-    }
-});
-xhr.open('GET', './fixtures/Pessoa03_MG.csv');
-xhr.send(null);
+fetch('./fixtures/Pessoa03_MG.csv')
+    .then(response => {
+        if (response.ok) {
+            return response.text();
+        }
+        throw new Error('could not load data file');
+    })
+    .then(body => {
+        addLayer(dsvFormat(';').parse(body));
+    });
